aa1e996d38d59599450220e84378def8e2f9d39f
Taoo <hai.alphacreative@gmail.com>
2026-01-24 16:33:16 +0700

    add loading

--- assets/js/index/loading.js
+++ assets/js/index/loading.js
@@ -1,23 +1,33 @@
 export function loading() {
   if (!document.querySelector("#loading")) return;
   const tl = gsap.timeline();
+
+  // Scale trước
   tl.to("#loading .loading-logo", {
-    opacity: 0,
     scale: 3.5,
     duration: 1.5,
-    ease: "power2.inOut"
+    ease: "power2.inOut",
   });
+
+  // Sau đó opacity
+  tl.to("#loading .loading-logo", {
+    opacity: 0,
+    duration: 0.5,
+    ease: "power2.inOut",
+  });
+
   tl.add(() => {}, "+=0.5");
+
   tl.fromTo(
     "#loading",
     {
-      clipPath: "inset(0% 0% 0% 0%)"
+      clipPath: "inset(0% 0% 0% 0%)",
     },
     {
       clipPath: "inset(0% 0% 100% 0%)",
       duration: 1,
-      ease: "power2.inOut"
-    }
+      ease: "power2.inOut",
+    },
   );
 
   return tl;
